3d535033d7045784ecfb7b5143537d779b7ca44f
Alex <AAArdyshev@yandex.ru>
2021-08-31 10:30:14 +0300

    make a form

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,24 @@
 import "./slider";
 import modals from "./modules/modals";
+import tabs from './modules/tabs'
+import forms from './modules/forms'
 
 window.addEventListener('DOMContentLoaded', ()=> {
-    modals();
+    'use strict';
+    const intervalId =  setInterval(() => {
+        document.querySelector('.popup').style.display = "block";
+        document.body.classList.add('modal-open');
+        console.log('ok');
+        if(document.body.classList.contains('modal-open')) {
+            clearInterval(intervalId)
+        }
+    }, 60000);
+
+    
+
+    modals(intervalId);
+
+    tabs('.glazing_slider','.glazing_block', '.glazing_content','active');
+    tabs('.decoration_slider','.no_click', '.decoration_content > div > div','after_click');
+    forms();
 });
